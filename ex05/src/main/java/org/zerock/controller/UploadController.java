99c8c4943ece85cfe0896b893b0da7e1b9229efe
somecreater <dbstjrgy123@gmail.com>
2023-10-27 13:57:52 +0900

    10월 27일자 스프링학습

--- ex05/src/main/java/org/zerock/controller/UploadController.java
+++ ex05/src/main/java/org/zerock/controller/UploadController.java
@@ -1,7 +1,15 @@
 package org.zerock.controller;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.extern.log4j.Log4j;
 
@@ -12,5 +20,79 @@ public class UploadController {
 	@GetMapping("/uploadForm")
 	public void uploadForm() {
 		log.info("upload form");
+	}
+	
+	@PostMapping("/uploadFormAction")
+	public void uploadFormPost(MultipartFile[] uploadFile, Model model) {
+		
+		String uploadFolder="D://upload";
+		
+		for(MultipartFile multipartFile:uploadFile) {
+			log.info("----------------------");
+			log.info("Upload File Name: "+multipartFile.getOriginalFilename());
+			log.info("Upload File Size: "+multipartFile.getSize());
+			
+			File saveFile=new File(uploadFolder,multipartFile.getOriginalFilename());
+			
+			try {
+				multipartFile.transferTo(saveFile);
+			}catch(Exception e) {
+				log.error(e.getMessage());
+			}
+		}
+	}
+
+	
+	@GetMapping("/uploadAjax")
+	public void uploadAjax() {
+		log.info("upload ajax");
+	}
+	
+	private String getFolder() {
+		
+		SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+		
+		Date date=new Date();
+		
+		String str=sdf.format(date);
+		return str.replace("-",File.separator);
+	}
+	@PostMapping("/uploadAjaxAction")
+	public void uploadAjaxPost(MultipartFile[] uploadFile) {
+		
+		log.info("update ajax post.........");
+		
+		String uploadFolder="D://upload";
+		
+		File uploadPath=new File(uploadFolder,getFolder());
+		log.info("upload path: "+uploadPath);
+		
+		if(uploadPath.exists()==false) {
+			uploadPath.mkdirs();
+		}
+		
+		for(MultipartFile multipartFile:uploadFile) {
+			log.info("--------------------------------");
+			log.info("Upload File Name: "+multipartFile.getOriginalFilename());
+			log.info("Upload File Size: "+multipartFile.getSize());
+			
+			String uploadFileName=multipartFile.getOriginalFilename();
+			
+			uploadFileName=uploadFileName.substring(uploadFileName.lastIndexOf("//")+1);
+			
+			log.info("only file name: "+uploadFileName);
+			
+			UUID uuid=UUID.randomUUID();
+			
+			uploadFileName=uuid.toString()+"_"+uploadFileName;
+			
+			File saveFile=new File(uploadPath,uploadFileName);
+			
+			try {
+				multipartFile.transferTo(saveFile);
+			}catch(Exception e) {
+				log.error(e.getMessage());
+			}
+		}
 	}
 }
